b7ef2c7db67f622547b8eb37c17f39ac022207ee
asiyuan <429141582@qq.com>
2019-01-16 20:15:18 +0800

    add header component

--- src/api/request.js
+++ src/api/request.js
@@ -2,7 +2,31 @@
  * Created by asy on 2019-1-15.
  */
 // 根据接口文档定义接口请求参数
+import {message} from 'antd'
+import jsonp from 'jsonp'
+
 import ajax from './ajax'
 
 // 处理登录的请求
 export const reqLogin = (user) => ajax('/login', user, 'POST')
+
+// 处理请求天气
+export function reqWeather(city) {
+  const url = `http://api.map.baidu.com/telematics/v3/weather?location=${city}&output=json&ak=3p49MVra6urFRGOT9s8UBWr2`
+
+  // 返回 promise 对象
+  return new Promise((resolve, reject) => {
+    console.log('reqWeather()')
+    jsonp(url, {
+      param: 'callback'
+    }, (err, response) => {
+      if (!err) {
+        const {dayPictureUrl, weather} = response.results[0].weather_data[0]
+        resolve({dayPictureUrl, weather})
+      } else {
+        message.error('请求天气出错了')
+      }
+    })
+  })
+}
+
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,14 +1,106 @@
 import React, {Component} from 'react';
+import {withRouter} from 'react-router-dom'
+import {Col, Row, Modal} from 'antd'
 
 import './header.less'
+import MemoryUtils from '../../utils/MemoryUtils'
+import storageUtils from '../../utils/storageUtils'
+import {formateDate} from '../../utils/utils'
+import {reqWeather} from '../../api/request'
+import menuList from '../../config/menuConfig'
 
-export default class Header extends Component {
+var title = null
+class Header extends Component {
+
+  state = {
+    sysTime: formateDate(Date.now()),
+    dayPictureUrl: '',
+    weather: ''
+  }
+
+  lagout = () => {
+    Modal.confirm({
+      title: 'Do you Want to exit?',
+      onOk: () => {
+        storageUtils.removeUser()
+        MemoryUtils.user = {}
+        this.props.history.replace('/login')
+      },
+      onCancel() {
+      },
+    })
+  }
+
+  getWeather = async () => {
+    const {dayPictureUrl, weather} = await reqWeather('北京')
+    this.setState({
+      dayPictureUrl,
+      weather
+    })
+  }
+
+  getSysTime = () => {
+    this.intervalId = setInterval(() => {
+      this.setState({
+        sysTime: formateDate(Date.now())
+      })
+    },1000)
+  }
+  getTitle = (menuList, path) => {
+    if (!menuList) {
+      return 
+    }
+    for (let i = 0; i < menuList.length; i++) {
+      if (menuList[i].key === path) {
+        title = menuList[i].title
+        break
+      }
+      if (menuList[i].children) {
+        const menu = menuList[i].children
+        this.getTitle(menu, path)
+      }
+    }
+    return title
+  }
+
+  // 异步的 ajax 请求放在 componentDidMount 中
+  componentDidMount() {
+    this.getWeather()
+    this.getSysTime()
+  }
+
+  // 在组件销毁前清除定时器
+  componentWillUnmount() {
+    clearInterval(this.intervalId)
+  }
 
   render() {
-    return (
-      <div className="header">
+    const user = MemoryUtils.user
+    const {dayPictureUrl, weather, sysTime} = this.state
 
+    // 得到标题
+    const path = this.props.location.pathname
+    const title = this.getTitle(menuList, path)
+    
+    return (
+      <div className='header'>
+        <Row className='header-top'>
+          <span>欢迎，{user.username}</span>
+          <a href="javascript:" onClick={this.lagout}>退出</a>
+        </Row>
+        <Row className='breadcrumb'>
+          <Col span={4} className='breadcrumb-title'>{title}</Col>
+          <Col span={20} className='weather'>
+            <span className='date'>{sysTime}</span>
+            <span className='weather-img'>
+              <img src={dayPictureUrl} alt="weather"/>
+            </span>
+            <span className='weather-detail'>{weather}</span>
+          </Col>
+        </Row>
       </div>
     )
   }
 }
+
+export default withRouter(Header)
